2bbf224e40e0ff5e30c8e09aeaf39fc6704c3cb9
Austin Reynolds <austin@austinreynolds.org>
2021-10-25 21:50:02 -0500

    basic tinkering

---
+++ emacs/init.el
@@ -0,0 +1,7 @@
+(setq inhibit-startup-message t)  ; Start straight into *scratch* buffer
+(setq visible-bell t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+;(load-theme 'misterioso)
